ca06f705f0abc02cb297978b3ad566444b54dabe
Chris <christopher.brownlie@education.gov.uk>
2019-07-18 13:51:31 +0100

    refactor: remove option for formattable output

--- R/common_words.R
+++ R/common_words.R
@@ -4,10 +4,9 @@
 #'
 #' @param data dataframe or tibble with a row per survey response
 #' @param column name of a character column in the data frame to be tabulated
-#' @param ... column(s) to split into groups (must be less than 20 unique combinations of values between columns). Defaults to no groups
+#' @param ... optional column(s) to split into groups (must be less than 20 unique combinations of values between columns).
 #' @param n number indicating how many most common words to return for each group. Defaults to 3
 #' @param min number indicating the minimum number of times a word needs to appear for it to be included in output, defaults to 5
-#' @param output one of either 'tb' (tibble), 'ft' (formattable) or 'pretty' (edited formattable). Defaults to 'tb'
 #' @param stopwords logical indicating whether to remove stopwords or not. Defaults to TRUE
 #' @param remove character vector of additional words to remove
 #'
@@ -17,7 +16,6 @@ common_words <- function(data,
                          ...,
                          n = 3,
                          min = 5,
-                         output = 'tb',
                          stopwords = TRUE,
                          remove = c("")) {
 
@@ -46,12 +44,7 @@ common_words <- function(data,
       dplyr::rename("Word" = word,
              "Count" = count)
 
-    if (output == 'tb') {
-      return(c_w)
-    } else if (output == 'ft') {
-      ft_c_w <- c_w %>%
-        formattable::formattable()
-      return(ft_c_w)
-    }
+
+    return(c_w)
 }
 
