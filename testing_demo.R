6977a9cacf1979429ad5a076e10cd9311d51d374
Chris <christopher.brownlie@education.gov.uk>
2019-07-19 16:54:43 +0100

    demo: add script for testing functions and demo at Labs Sprint 1 retrospective

---
+++ testing_demo.R
@@ -0,0 +1,154 @@
+# setup --------------------------------------------------------------------------------------------------------
+
+# Load dplyr to assist
+library(dplyr)
+
+# Import raw data
+data <- readxl::read_xlsx("../demo_data.xlsx")
+
+# Show questions
+names(data)
+
+# Transform into clean format
+new_data <- data %>%
+  rename("user" = 1,
+         "q1_position_in_trust" = 2,
+         "q2_rsc_office_met" = 3,
+         "q12_main_challenges" = 4,
+         "q15_not_expectations" = 5,
+         "q22_changes" = 6,
+         "q24_reflections" = 7)
+
+# Focussing on q24_reflections, take a look at some of the data for comparisons later
+sample <- 27:37
+new_data[sample,"q24_reflections"]
+
+# anonymise() ---------------------------------------------------------------------------------------
+
+# See names 'Joe Bloggs' and 'John Doe' - column needs to be anonymised
+# Default behaviour
+anon_data <- new_data %>%
+  anonymise(q24_reflections)
+
+anon_data[sample,"q24_reflections"]
+
+# If we want to distinguish between the different people (i.e. for analysis, see if one person mentioned alot etc.)
+anon_data <- new_data %>%
+  anonymise(q24_reflections,
+            identify = TRUE)
+
+anon_data[sample,"q24_reflections"] # Random numbers assigned
+
+# If we want to make the column gender neutral as well
+anon_data <- new_data %>%
+  anonymise(q24_reflections,
+            identify = TRUE,
+            gender = TRUE)
+
+anon_data[sample,"q24_reflections"]
+
+# If we want to anonymise certain names (i.e. we have a list of certain names)
+names_to_anonymise <- c("Joe Bloggs", "the NSC")
+anon_data <- new_data %>%
+  anonymise(q24_reflections,
+            auto = FALSE, # So that the function doesn't automatically overwrite every name it finds
+            add_names = names_to_anonymise, # Manually defined names to anonymise
+            identify = TRUE,
+            gender = TRUE)
+
+anon_data[sample,"q24_reflections"]
+
+
+# clean_column() -----------------------------------------------------------------------------------------
+# After anonymising, column needs to be standardised (lower case, punctuation etc.) before it can be analysed
+clean_data <- anon_data %>%
+  clean_column(q24_reflections)
+
+clean_data[sample,"q24_reflections"]
+
+# If we want to keep 'null responses'
+clean_data <- anon_data %>%
+  clean_column(q24_reflections,
+               null_response = FALSE)
+
+clean_data[sample,"q24_reflections"]
+
+
+# common_words() --------------------------------------------------------------------------------------------
+# Simple frequency analysis, filters out words that appear less than 5 times so
+# filters out groups with only a couple of respondents
+clean_data %>%
+  common_words(q24_reflections)
+
+# More words
+clean_data %>%
+  common_words(q24_reflections,
+               n = 10)
+
+# Remove certain words
+clean_data %>%
+  common_words(q24_reflections,
+               remove = c("meeting", "rsc", "trust"),
+               n = 10)
+
+# Breakdown by demographic
+clean_data %>%
+  common_words(q24_reflections,
+               q1_position_in_trust,
+               remove = c("meeting", "rsc", "trust"),
+               n = 1)
+
+# To include all groups
+clean_data %>%
+  common_words(q24_reflections,
+               q1_position_in_trust,
+               min = 0)
+
+# Stopwords are currently being removed, to keep them in
+clean_data %>%
+  common_words(q24_reflections,
+               q1_position_in_trust,
+               stopwords = FALSE)
+
+# Can also calculate the proportion of responses in each group
+# that mentioned the word
+clean_data %>%
+  common_words(q24_reflections,
+               q1_position_in_trust,
+               proportion = TRUE)
+
+# Finally, if exporting to external document, can make it more visually appealing with prettify()
+clean_data %>%
+  common_words(q24_reflections,
+               q1_position_in_trust,
+               proportion = TRUE,
+               remove = c("meeting", "rsc", "trust")) %>%
+  prettify(alias = c("Respondent's position in Trust" = "q1_position_in_trust"),
+           count_bar = TRUE,
+           colour_groups = TRUE)
+
+# n_grams() --------------------------------------------------------------------------------------------------
+# Method of tabulating most common n-grams, removing n_grams that are mostly stopwords
+clean_data %>%
+  n_grams(q24_reflections) # defaults to bigrams
+
+# Can decrease the threshold to be stricter on stopwords
+clean_data %>%
+  n_grams(q24_reflections,
+          stop_thresh = 0) # 0 means exclude n-grams that contain one or more stopwords, 1 means include all n-grams
+
+# Other options
+clean_data %>%
+  n_grams(q24_reflections,
+          n = 3, # trigrams
+          word = "meeting", # filter to only include n-grams containing the word 'meeting'
+          stop_thresh = 0.4) # allow only one stopword per trigram
+
+# Prettify
+clean_data %>%
+  n_grams(q24_reflections,
+          n = 3, # trigrams
+          word = "meeting", # filter to only include n-grams containing the word 'better'
+          stop_thresh = 0.4) %>% # allow only one stopword per ngram
+  prettify(alias = c("Phrase" = "ngram"),
+           count_bar = TRUE)
