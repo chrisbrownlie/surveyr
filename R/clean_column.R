94407b89080491af8e32d30d00715de2a065b100
Chris <christopher.brownlie@education.gov.uk>
2019-07-31 14:42:02 +0100

    fix: sort some issues in the clean_column function in preparation for sentiment_score function

--- R/clean_column.R
+++ R/clean_column.R
@@ -5,28 +5,29 @@
 #' @param data dataframe or tibble with a row per survey response
 #' @param column name of a character column in the data frame to be cleaned
 #' @param num logical indicating whether numbers should be removed as part of cleaning process
+#' @param null_response logical indicating whether to remove non-answers (e.g. 'no', 'none', 'na' etc.)
 #'
 #' @return Original dataframe with text column cleaned and standardised ready for analysing
 clean_column <- function(data,
                          column,
-                         num = TRUE,
+                         num = FALSE,
                          null_response = TRUE) {
 
   column <- dplyr::enquo(column)
 
   new_col <- dplyr::pull(data, {{ column }}) %>%
-    stringr::str_replace_all(pattern = "[^[:alnum:][:space:]\\[\\]]", replacement = "") %>%
+    stringr::str_replace_all(pattern = "[^[:alnum:][:space:]\\[\\]\\.\\?]", replacement = "") %>%
     stringr::str_replace_all(pattern = "[[:space:]]+", replacement = " ") %>%
     tolower()
 
   if (num == TRUE) {
     new_col <- new_col %>%
-      stringr::str_replace_all(pattern = "[[:digit:]]([^//]])", replacement = "//1")
+      stringr::str_replace_all(pattern = "[[:space:]][[:digit:]]+", replacement = "[number]")
   }
 
   if (null_response == TRUE) {
     new_col <- new_col %>%
-      stringr::str_replace_all(pattern = "^no$|^nothing$|^na$", replacement = "")
+      stringr::str_replace_all(pattern = "^(no|nothing|none|na|not really|no thanks|no thank you)(\\.)?[[:space:]]*$", replacement = "")
   }
 
   data[[dplyr::quo_name(column)]] <- new_col
