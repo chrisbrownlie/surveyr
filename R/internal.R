639e09ffc45ef6db671f8d6075e629421c85323e
Chris <christopher.brownlie@education.gov.uk>
2019-08-07 14:06:36 +0100

    fix: fix bug in internal function

--- R/internal.R
+++ R/internal.R
@@ -1,7 +1,7 @@
 # function for loading data and getting vector of items included
 load_internal_data <- function() {
 
-  internalnames <<- load("R/sysdata.rda")
-  load("R/sysdata.rda", envir = globalenv())
+  eval(quote(internalnames <- load("R/sysdata.rda")), envir = parent.frame())
+  load("R/sysdata.rda", envir = parent.frame())
 
 }
