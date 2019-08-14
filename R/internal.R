bc7b070fa9260f0de6573bd0dae65c66ea1f5573
Chris <christopher.brownlie@education.gov.uk>
2019-08-14 12:17:51 +0100

    fix: update sysdata

--- R/internal.R
+++
@@ -1,7 +0,0 @@
-# function for loading data and getting vector of items included
-load_internal_data <- function() {
-
-  eval(quote(internalnames <- load("R/sysdata.rda")), envir = parent.frame())
-  load("R/sysdata.rda", envir = parent.frame())
-
-}
