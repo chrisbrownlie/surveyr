1ca3fc4e4728aef451211911d210e99a2896359c
Chris <christopher.brownlie@education.gov.uk>
2019-09-25 10:25:55 +0100

    fix: removed erroneous start-up function

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,3 @@
-.onLoad <- function(libname, pkgname) {
-
-  topicnames <- paste0("topic", 1:100)
-  save(topicnames, file = "R/sysdata.rda")
-
-}
-
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to the surveyr package v0.1 (Aug-2019). For more information contact christopher.brownlie@education.gov.uk")
+  packageStartupMessage("This is surveyr v0.1.1 (Sep-2019). For information contact christopher.brownlie@education.gov.uk")
 }
