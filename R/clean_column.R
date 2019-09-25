b09af858915a21fa9188e9458589645b00694901
Chris <christopher.brownlie@education.gov.uk>
2019-09-25 11:42:47 +0100

    feat: rename clean_column function to clean(), and add clean_all and clean_at functions

--- R/clean_column.R
+++ R/clean_column.R
@@ -1,6 +1,5 @@
 #' Function to clean single text column, remove punctuation and standardise
 #'
-#'
 #' @param data dataframe or tibble with a row per survey response
 #' @param column name of a character column in the data frame to be cleaned
 #' @param num logical indicating whether numbers should be removed as part of cleaning process
@@ -9,10 +8,10 @@
 #' @return Original dataframe with text column cleaned and standardised ready for analysing
 #'
 #' @export
-clean_column <- function(data,
-                         column,
-                         num = FALSE,
-                         null_response = TRUE) {
+clean <- function(data,
+                  column,
+                  num = FALSE,
+                  null_response = TRUE) {
 
   column <- enquo(column)
 
@@ -33,4 +32,54 @@ clean_column <- function(data,
 
   data[[quo_name(column)]] <- new_col
   return(data)
+}
+
+#' Function to apply clean() function to all columns
+#'
+#' @param data dataframe or tibble with a row per survey response
+#' @param num logical indicating whether numbers should be removed as part of cleaning process
+#' @param null_response logical indicating whether to remove non-answers (e.g. 'no', 'none', 'na' etc.)
+#'
+#' @return Original dataframe with all columns cleaned and standardised ready for analysing
+#'
+#' @export
+clean_all <- function(data,
+                      num = FALSE,
+                      null_response = TRUE) {
+
+  new_data <- purrr::reduce(.x = names(data),
+                            .f = clean,
+                            .init = data,
+                            num = num,
+                            null_response = null_response)
+
+  return(new_data)
+}
+
+#' Function to apply clean() function to multiple specified columns
+#'
+#' @param data dataframe or tibble with a row per survey response
+#' @param ... columns for the clean() function to be applied to
+#' @param num logical indicating whether numbers should be removed as part of cleaning process
+#' @param null_response logical indicating whether to remove non-answers (e.g. 'no', 'none', 'na' etc.)
+#'
+#' @return Original dataframe with specified columns cleaned and standardised ready for analysing
+#'
+#' @export
+clean_at <- function(data,
+                     ...,
+                     num = FALSE,
+                     null_response = TRUE) {
+
+  cols <- purrr::map(enquos(...), quo_name) %>%
+    unlist()
+
+  new_data <- purrr::reduce(.x = cols,
+                            .f = clean,
+                            .init = data,
+                            num = num,
+                            null_response = null_response)
+
+  return(new_data)
+
 }
