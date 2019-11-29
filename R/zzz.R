4f1c6e38b9f4f7a5e85f5fc2b95342d6cf70ae00
Chris <christopher.brownlie@education.gov.uk>
2019-11-29 10:15:29 +0000

    docs: version update

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("This is surveyr v0.0.4 (Nov-2019). For information contact christopher.brownlie@education.gov.uk")
+  packageStartupMessage("This is surveyr v0.1.0 (Nov-2019). For information contact christopher.brownlie@education.gov.uk")
 }
